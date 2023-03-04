0eafeb66158cd65efc7669226a52079b933f4bce
mariaafrv <mariaafv@hotmail.com>
2023-03-04 13:57:21 -0300

    primeiro commit

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import "./style.css"
+
+function CardGame() {
+    return `
+        <article class="card-game">
+            <img src="images/zoom.png" class="card-logo">
+        </article>
+    `
+}
+
+export default CardGame;
