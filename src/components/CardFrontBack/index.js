3bcd5313c7dd29cd330aba4e520146c032e4ed17
mariaafrv <mariaafv@hotmail.com>
2023-04-01 15:53:58 -0300

    Adicionando animacao ao card

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,10 +1,15 @@
 import CardGame from "../CardGame";
+import "./style.css"
 
 function CardFrontBack() {
     return /*html*/ `
         <article class="card-front-back">
-            ${CardGame()}
-            ${CardGame("britney-fly", "Icone Britney Spears comissária")}
+            <div class="card -front">
+                ${CardGame()}
+            </div>
+            <div class="card -back">
+                ${CardGame("britney-fly", "Icone Britney Spears comissária")}
+            </div>
         </article>
     `
 }
